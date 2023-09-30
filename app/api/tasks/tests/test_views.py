7d2ae1de82f197cbe32a508c2759c9b0a8274466
Braulio Hernández Chaparro <brauliohrdz@gmail.com>
2023-09-30 16:41:46 +0100

    Añadir restriccion al listado de tareas - Solo los usuarios registrados pueden acceder.

--- app/api/tasks/tests/test_views.py
+++ app/api/tasks/tests/test_views.py
@@ -1,4 +1,5 @@
 from api.tasks.views import TasksList
+from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APITestCase
 
@@ -6,19 +7,31 @@ from rest_framework.test import APITestCase
 class TasksListViewTextCase(APITestCase):
     endpoint_url = "/api/v1/tasks/list/"
 
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.user = User.objects.create(username="admin", email="admin@example.com")
+
     def test_view_url(self):
+        self.client.force_authenticate(self.user)
         response = self.client.get(self.endpoint_url)
         self.assertNotEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         self.assertIs(response.resolver_match.func.view_class, TasksList)
 
     def test_post_method_is_not_allowed(self):
+        self.client.force_authenticate(self.user)
         response = self.client.post(self.endpoint_url)
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_put_method_is_not_allowed(self):
+        self.client.force_authenticate(self.user)
         response = self.client.put(self.endpoint_url)
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_delete_method_is_not_allowed(self):
+        self.client.force_authenticate(self.user)
         response = self.client.delete(self.endpoint_url)
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_login_is_required(self):
+        response = self.client.get(self.endpoint_url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
--- app/api/tasks/views.py
+++ app/api/tasks/views.py
@@ -1,8 +1,13 @@
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
 class TasksList(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         return Response(status=status.HTTP_204_NO_CONTENT)
