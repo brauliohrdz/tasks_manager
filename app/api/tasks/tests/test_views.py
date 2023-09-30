780452a5cf270b2468f240a12ef7b8fd34a6347c
Braulio Hernández Chaparro <brauliohrdz@gmail.com>
2023-09-30 16:28:57 +0100

    Vista del listado de tareas solo admite get - Test para comprobar que únicamente están permitidas las peticiones get a esta vista.

--- app/api/tasks/tests/test_views.py
+++ app/api/tasks/tests/test_views.py
@@ -10,3 +10,15 @@ class TasksListViewTextCase(APITestCase):
         response = self.client.get(self.endpoint_url)
         self.assertNotEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         self.assertIs(response.resolver_match.func.view_class, TasksList)
+
+    def test_post_method_is_not_allowed(self):
+        response = self.client.post(self.endpoint_url)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_put_method_is_not_allowed(self):
+        response = self.client.put(self.endpoint_url)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_delete_method_is_not_allowed(self):
+        response = self.client.delete(self.endpoint_url)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
