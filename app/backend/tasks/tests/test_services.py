e0a0d085b8af3fca39fa02714917a4ed30c0c94f
Braulio Hernández Chaparro <brauliohrdz@gmail.com>
2023-10-02 20:57:50 +0100

    Refactorización de los test para el servicio de listado de tareas.

--- app/backend/tasks/tests/test_services.py
+++ app/backend/tasks/tests/test_services.py
@@ -1,8 +1,9 @@
-from backend.tasks.models import Task
 from backend.tasks.services import list_tasks_for_user
 from django.contrib.auth.models import User
 from django.db.models import QuerySet
 from django.test import TestCase
+
+from .utils import TaskTestUtils
 
 
 class ListTasksForUserTestCase(TestCase):
@@ -20,12 +21,20 @@ class ListTasksForUserTestCase(TestCase):
 
     def test_service_list_user_tasks_only(self):
         another_user = User.objects.create(username="nedflanders@example.com")
-        Task.objects.create(id=1, title="No listable task", owner=another_user)
-        Task.objects.create(id=2, title="Homer task1", owner=self.user)
-        Task.objects.create(id=3, title="Homer task2", owner=self.user)
+        TaskTestUtils.create(id=1, title="No listable task", owner=another_user)
+        TaskTestUtils.create(id=2, title="Homer task1", owner=self.user)
+        TaskTestUtils.create(id=3, title="Homer task2", owner=self.user)
 
-        expected_tasks_ids = [2, 3]
+        expected_tasks_ids_for_user = [2, 3]
 
         tasks = list_tasks_for_user(id=self.user.id)
-        retrieved_tasks_ids = list(tasks.values_list("id", flat=True))
-        self.assertListEqual(expected_tasks_ids, retrieved_tasks_ids)
+        retrieved_tasks_ids_for_user = list(tasks.values_list("id", flat=True))
+        self.assertListEqual(expected_tasks_ids_for_user, retrieved_tasks_ids_for_user)
+
+        expected_tasks_ids_for_another_user = [1]
+
+        tasks = list_tasks_for_user(id=another_user.id)
+        retrieved_tasks_ids_for_another_user = list(tasks.values_list("id", flat=True))
+        self.assertListEqual(
+            expected_tasks_ids_for_another_user, retrieved_tasks_ids_for_another_user
+        )
