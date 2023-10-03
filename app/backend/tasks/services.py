b6a70d4405e8d43bf61fc422011c166fc1045560
Braulio Hernández Chaparro <brauliohrdz@gmail.com>
2023-10-03 23:47:20 +0100

    Refactorizar servicios.

--- app/backend/tasks/services.py
+++ app/backend/tasks/services.py
@@ -3,27 +3,32 @@ from django.core.exceptions import FieldError
 from django.db.models import QuerySet
 
 
+def get_task_for_owner(task_uuid: str, owner_id: int):
+    task = Task.objects.get(uuid=task_uuid)
+    is_task_owner = task.owner_id == owner_id
+    if is_task_owner:
+        return task
+    raise PermissionError("User is not task owner.")
+
+
 def list_tasks_for_user(id: int) -> QuerySet[Task]:
     assert id, "User id is required."
     return Task.objects.filter(owner_id=id)
 
 
-def create_task(owner_id: int, **kwargs) -> None:
+def create_task(owner_id: int, **kwargs) -> Task:
     assert owner_id, "Owner id is required."
     task = Task(owner_id=owner_id, **kwargs)
     task.full_clean()
     task.save()
+    return task
 
 
 def update_task(task_uuid: int, owner_id: int, **kwargs) -> None:
     assert task_uuid, "Task uuid is required."
     assert owner_id, "Owner id is required."
 
-    task = Task.objects.get(uuid=task_uuid)
-    is_task_owner = task.owner_id == owner_id
-    if not is_task_owner:
-        raise PermissionError("User is not task owner.")
-
+    task = get_task_for_owner(task_uuid=task_uuid, owner_id=owner_id)
     editable_fields = Task.editable_fields()
     all_fields_are_editable = all([field in editable_fields for field in kwargs.keys()])
     if not all_fields_are_editable:
@@ -43,8 +48,15 @@ def delete_task(task_uuid: int, owner_id: int, **kwargs) -> None:
     assert task_uuid, "Task uuid is required."
     assert owner_id, "Owner id is required."
 
-    task = Task.objects.get(uuid=task_uuid)
-    is_task_owner = task.owner_id == owner_id
-    if not is_task_owner:
-        raise PermissionError("User is not task owner.")
+    task = get_task_for_owner(task_uuid=task_uuid, owner_id=owner_id)
     task.delete()
+
+
+# def add_task_image(task_uuid: int, owner_id: int, image) -> None:
+#     assert task_uuid, "Task uuid is required."
+#     assert owner_id, "Owner id is required."
+
+#     task = Task.objects.get(uuid=task_uuid)
+#     task_image = TaskImage(image=image, task_id=task.id)
+#     task_image.full_clean()
+#     task_image.save()
