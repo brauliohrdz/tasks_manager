b1b8eb0d1ef20ff29787ea561385802c9f62aa94
Braulio Hernández Chaparro <brauliohrdz@gmail.com>
2023-10-03 17:42:24 +0100

    Task puede listar sus campos editables

--- app/backend/tasks/models.py
+++ app/backend/tasks/models.py
@@ -6,13 +6,15 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Task(models.Model):
-    uuid = models.UUIDField(unique=True, default=uuid4)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    uuid = models.UUIDField(unique=True, default=uuid4, editable=False)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    updated = models.DateTimeField(auto_now=True, editable=False)
     expires = models.DateTimeField(null=True, blank=True)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    owner = models.ForeignKey(User, related_name="tasks", on_delete=models.PROTECT)
+    owner = models.ForeignKey(
+        User, related_name="tasks", on_delete=models.PROTECT, editable=False
+    )
 
     PENDING = "pending"
     IN_PROGRESS = "in_progress"
@@ -24,6 +26,13 @@ class Task(models.Model):
         (COMPLETED, _("Completada")),
     )
     status = models.CharField(choices=STATUS_CHOICES, max_length=20)
+
+    @classmethod
+    def editable_fields(cls) -> list[str]:
+        editable_fields = list(
+            filter(lambda f: f.editable and not f.primary_key, cls._meta.fields)
+        )
+        return [field.name for field in editable_fields]
 
     class Meta:
         verbose_name = "Task"
