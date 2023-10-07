c7a8141b3bfc78a76150a896e15282f71896c937
Braulio Hernández Chaparro <brauliohrdz@gmail.com>
2023-10-07 01:39:34 +0100

    Test para la eliminación de imágenes

--- app/frontend/tasks/tests/test_tasks_images_views.py
+++ app/frontend/tasks/tests/test_tasks_images_views.py
@@ -4,7 +4,7 @@ from backend.tasks.tests.utils import TaskImageTestUtils, TaskTestUtils
 from backend.users.tests.utils import UserTestUtils
 from django.test import TestCase
 from django.urls import reverse
-from frontend.tasks.views import CreateTaskImage
+from frontend.tasks.views import CreateTaskImage, DeleteTaskImage
 
 
 class CreateTaskImageViewTestCase(TestCase):
@@ -32,13 +32,13 @@ class CreateTaskImageViewTestCase(TestCase):
         response = self.client.post(self.view_url)
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
 
-    def test_non_existent_task_uuid_returns_404(self):
+    def test_non_existent_task_uuid_returns_400(self):
         self.client.force_login(self.user)
         image = TaskImageTestUtils.simple_uploaded_image()
         response = self.client.post(
             self.view_url, data={"image": image}, format="multipart"
         )
-        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
 
     def test_no_task_owner_cannot_add_images_to_task(self):
         TaskTestUtils.create(title="My task", uuid=self.test_uuid)
@@ -66,3 +66,53 @@ class CreateTaskImageViewTestCase(TestCase):
                 task__uuid=self.test_uuid, task__owner_id=self.user.id
             )
         )
+
+
+class DeleteTaskImageViewTestCase(TestCase):
+    test_uuid = "21fb4127-c9e6-4040-8684-dceeeb8eb816"
+    view_url = f"/tasks/images/delete/{test_uuid}/"
+    view_name = "tasks_image_delete"
+
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.user = UserTestUtils.create()
+
+    def test_view_url(self):
+        self.client.force_login(self.user)
+        response = self.client.get(self.view_url)
+        self.assertNotEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        self.assertIs(response.resolver_match.func.view_class, DeleteTaskImage)
+
+    def test_view_name(self):
+        self.client.force_login(self.user)
+        url = reverse(self.view_name, args=[self.test_uuid])
+        response = self.client.get(url)
+        self.assertNotEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        self.assertIs(response.resolver_match.func.view_class, DeleteTaskImage)
+
+    def test_login_required(self):
+        response = self.client.get(self.view_url)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+
+    def test_non_existent_task_image_uuid_returns_400(self):
+        self.client.force_login(self.user)
+        response = self.client.get(self.view_url)
+        self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
+
+    def test_non_task_owner_cannot_delete_task_image(self):
+        TaskImageTestUtils.create(uuid=self.test_uuid)
+        self.client.force_login(self.user)
+        response = self.client.get(self.view_url)
+        self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
+
+    def test_user_can_delete_task_image(self):
+        task = TaskTestUtils.create(
+            title="My Task", owner_id=self.user.id, uuid=self.test_uuid
+        )
+        TaskImageTestUtils.create(uuid=self.test_uuid, task_id=task.id)
+        self.assertIsNotNone(TaskImageTestUtils.first(uuid=self.test_uuid))
+        self.client.force_login(self.user)
+        response = self.client.get(self.view_url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertIsNone(TaskImageTestUtils.first(uuid=self.test_uuid))
