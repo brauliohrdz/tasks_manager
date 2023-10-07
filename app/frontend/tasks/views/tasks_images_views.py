23950814459cb41b081e03283cf17deaa74dedb3
Braulio Hernández Chaparro <brauliohrdz@gmail.com>
2023-10-07 01:40:48 +0100

    Vistas para la gestión de imágenes - LoginRequired - Modificar not found por bad request cuando se indica una imagen incorrecta.

--- app/frontend/tasks/views/tasks_images_views.py
+++ app/frontend/tasks/views/tasks_images_views.py
@@ -1,11 +1,12 @@
 from backend.tasks.services import create_task_image, delete_task_image
 from django import forms
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import (
     HttpResponse,
+    HttpResponseBadRequest,
     HttpResponseForbidden,
-    HttpResponseNotFound,
     HttpResponseRedirect,
 )
 from django.urls import reverse
@@ -16,7 +17,7 @@ class TaskImageForm(forms.Form):
     image = forms.ImageField()
 
 
-class CreateTaskImage(View):
+class CreateTaskImage(LoginRequiredMixin, View):
     def post(self, request, task_uuid):
         try:
             form = TaskImageForm(request.POST, request.FILES)
@@ -31,17 +32,17 @@ class CreateTaskImage(View):
                 messages.error(request, form.errors)
             return HttpResponseRedirect(reverse("tasks_update", args=[task_uuid]))
         except ObjectDoesNotExist:
-            return HttpResponseNotFound("No se ha encontrado la tarea indicada")
+            return HttpResponseBadRequest("No se ha encontrado la tarea indicada")
         except PermissionError:
             return HttpResponseForbidden("No tiene permisos para realizar esta acción")
 
 
-class DeleteTaskImage(View):
+class DeleteTaskImage(LoginRequiredMixin, View):
     def get(self, request, task_image_uuid):
         try:
             delete_task_image(task_image_uuid=task_image_uuid, owner_id=request.user.id)
             return HttpResponse("")
         except ObjectDoesNotExist:
-            return HttpResponseNotFound("No se ha encontrado la tarea indicada")
+            return HttpResponseBadRequest("No se ha encontrado la tarea indicada")
         except PermissionError:
             return HttpResponseForbidden("No tiene permisos para realizar esta acción")
