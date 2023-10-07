78e6abd7ea8944a92c30cb6283aaeb2ecae65d98
Braulio Hernández Chaparro <brauliohrdz@gmail.com>
2023-10-07 02:54:15 +0100

    Instalar django filters

--- app/project/settings.py
+++ app/project/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework.authtoken",
+    "django_filters",
     "backend.tasks",
     "backend.users",
     "frontend.users",
